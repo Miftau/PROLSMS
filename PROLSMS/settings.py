f5cf9d431ef82acafc4492fc7f347aaeeb6c2f15
Miftau <miftaudeenb@gmail.com>
2025-04-26 02:39:43 +0100

    settings updated

--- PROLSMS/settings.py
+++ PROLSMS/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
