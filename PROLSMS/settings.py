3eb0470f86b818b6582814c3f8208b3aed2659d0
Miftau <miftaudeenb@gmail.com>
2025-04-26 02:54:04 +0100

    settings updated

--- PROLSMS/settings.py
+++ PROLSMS/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 LOGIN_REDIRECT_URL = '/dashboard/'
